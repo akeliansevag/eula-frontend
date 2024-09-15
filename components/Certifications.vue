9fa92bb1d750e2b3c7f2417b96a6e444efc729b5
Sevag Akelian <akelian.sevag@gmail.com>
2024-09-15 12:14:23 +0300

    .

--- components/Certifications.vue
+++ components/Certifications.vue
@@ -2,16 +2,16 @@
     <section class="bg-white py-10 lg:py-20">
         <div class="container">
             <h2 class="text-3xl font-semibold max-md:text-center">Supplier Certifications</h2>
-            <div class="flex max-md:justify-normal max-md:flex-wrap gap-2 justify-between items-center mt-10">
-                <div><img src="/public/img/ce.webp" alt="CE"></div>
-                <div><img src="/public/img/ecas.webp" alt="ECAS"></div>
-                <div><img src="/public/img/fda.webp" alt="FDA"></div>
-                <div><img src="/public/img/iso.webp" alt="ISO"></div>
-                <div><img src="/public/img/nsf.webp" alt="NSF"></div>
-                <div><img src="/public/img/ul.webp" alt="UL"></div>
-                <div><img src="/public/img/rohs.webp" alt="ROHS"></div>
-                <div><img src="/public/img/water-quality.webp" alt="Water Quality"></div>
-                <div><img src="/public/img/wras.webp" alt="WRAS"></div>
+            <div class="flex max-md:justify-center max-md:flex-wrap gap-2 justify-between items-center mt-10">
+                <div class="max-lg:max-w-[95px]"><img src="/public/img/ce.webp" alt="CE"></div>
+                <div class="max-lg:max-w-[95px]"><img src="/public/img/ecas.webp" alt="ECAS"></div>
+                <div class="max-lg:max-w-[95px]"><img src="/public/img/fda.webp" alt="FDA"></div>
+                <div class="max-lg:max-w-[95px]"><img src="/public/img/iso.webp" alt="ISO"></div>
+                <div class="max-lg:max-w-[95px]"><img src="/public/img/nsf.webp" alt="NSF"></div>
+                <div class="max-lg:max-w-[95px]"><img src="/public/img/ul.webp" alt="UL"></div>
+                <div class="max-lg:max-w-[95px]"><img src="/public/img/rohs.webp" alt="ROHS"></div>
+                <div class="max-lg:max-w-[95px]"><img src="/public/img/water-quality.webp" alt="Water Quality"></div>
+                <div class="max-lg:max-w-[95px]"><img src="/public/img/wras.webp" alt="WRAS"></div>
             </div>
         </div>
 
--- components/Partners.vue
+++ components/Partners.vue
@@ -1,18 +1,18 @@
 <template>
     <section class="bg-white py-10 lg:py-20">
         <div class="container">
-            <h2 class="text-3xl font-semibold">Partners</h2>
-            <div class="flex max-md:justify-normal max-md:flex-wrap gap-4 justify-between items-center mt-4">
-                <div>
+            <h2 class="text-3xl font-semibold max-lg:text-center">Partners</h2>
+            <div class="flex max-md:justify-center max-md:flex-wrap gap-4 justify-between items-center mt-4">
+                <div class="max-lg:max-w-[100px]">
                     <img src="/public/img/awani.webp" alt="Awani">
                 </div>
-                <div>
+                <div class="max-lg:max-w-[100px]">
                     <img src="/public/img/catch.webp" alt="Catch">
                 </div>
-                <div>
+                <div class="max-lg:max-w-[100px]">
                     <img src="/public/img/operation.webp" alt="operation">
                 </div>
-                <div>
+                <div class="max-lg:max-w-[100px]">
                     <img src="/public/img/kitopi.webp" alt="Kitopi">
                 </div>
 
